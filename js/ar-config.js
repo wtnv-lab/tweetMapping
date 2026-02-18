d1e8982525a02870d346739fc56e27e8cbc99d9c
Hidenori Watanave <hwtnv2006@gmail.com>
2026-02-18 15:02:12 +0900

    Hide debug toggle in AR config

--- js/ar-config.js
+++ js/ar-config.js
@@ -21,7 +21,7 @@ window.AR_CONFIG = {
     useTestLocationByDefault: false,
 
     // DEBUGスイッチUIを表示するか
-    showToggle: true,
+    showToggle: false,
 
     // DEBUGスイッチON時に使う固定座標
     // 仙台市役所付近: 緯度 38.268721, 経度 140.869407
